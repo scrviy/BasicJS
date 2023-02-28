7035385ae08250cc4e84ef50c47931fd5d1dd543
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-28 16:53:17 -0300

    Básico do Typescript

---
+++ console/index.js
@@ -0,0 +1,6 @@
+console.clear();
+console.log("'Olá!'") // String = Texto - Cadeia de caracteres
+console.log('Hello, "World!"')
+console.log(`'Bem' "vindo!"`);
+
+console.log(35, 15.99, "chuva do caralho");
