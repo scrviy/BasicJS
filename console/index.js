dc7bd94b47e1ae9fec37c9867e0abffe7385b668
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-28 16:59:13 -0300

    Básico do Javascript

--- console/index.js
+++ console/index.js
@@ -3,4 +3,4 @@ console.log("'Olá!'") // String = Texto - Cadeia de caracteres
 console.log('Hello, "World!"')
 console.log(`'Bem' "vindo!"`);
 
-console.log(35, 15.99, "chuva do caralho");
+console.log(35, 15.99, "chuva");
